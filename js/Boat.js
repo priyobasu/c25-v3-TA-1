ff756033c28c9b338f03ae5e38e9ce32578b1ca8
priyobasu <72121579+priyobasu@users.noreply.github.com>
2021-11-28 08:03:05 +0530

    Add files via upload

---
+++ js/Boat.js
@@ -0,0 +1,35 @@
+class Boat{
+constructor(x,y,width,height,boatPos){
+
+    var options={
+        restitution:0.8,
+        friction:1.0,
+        density:1.0
+    }
+
+    this.body=Bodies.rectangle(x,y,width,height,options);
+    this.width=width;
+    this.height=height;
+    this.image=loadImage("assets/boat.png");
+    this.boatPosition=boatPos;
+    World.add(world,this.body);
+
+
+}
+
+display(){
+
+    var pos=this.body.position;
+    var angle=this.body.angle;
+    
+    push();
+    translate(pos.x,pos.y);
+    rotate(angle);
+imageMode(CENTER);
+   image(this.image,0,this.boatPosition,this.width,this.height);
+
+    pop();
+}
+
+
+}
